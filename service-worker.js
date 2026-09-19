bd6c7debe14ebb416d17ca33a687503d56fbbb32
eps-mignot <florianmignot@gmail.com>
2026-09-19 11:11:49 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
    cache-first avec repli réseau, pour un fonctionnement 100% hors-ligne
    une fois la première visite effectuée.
    ========================================================================== */
-const CACHE_NAME = "carnet-eps-v6";
+const CACHE_NAME = "carnet-eps-v7";
 
 const APP_SHELL = [
   "./",
